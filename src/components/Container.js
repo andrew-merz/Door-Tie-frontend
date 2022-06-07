76bf32926c7a937eaa795cc9731721aaa9ecad31
andrew-merz <merz.school@gmail.com>
2022-06-06 20:22:40 -0400

    components added

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+function Container(props) {
+  return (
+    <container className="container">
+      <div>
+        <h1>this is where the status will go</h1>
+        <h1>this is where the status will go</h1>
+        <h1>this is where the status will go</h1>
+      </div>
+    </container>
+  );
+}
+
+export default Container;
