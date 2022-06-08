793d954b2ded17e7edd3e48636d05f2d93ef2aad
andrew-merz <merz.school@gmail.com>
2022-06-08 11:22:34 -0400

    frontend sending data to backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Nav from "./components/Nav";
-import Container from "./components/Container";
+
 function App() {
   return (
     <div className="App">
       <Nav />
       <h1>Hello World</h1>
-      <Container />
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,11 +6,29 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  async function registerUser(event) {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:4000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div>
       <Nav />
       <h1>Register</h1>
-      <form>
+      <form onSubmit={registerUser}>
         <input
           value={username}
           onChange={(e) => setUsename(e.target.value)}
